324a01e55e7cf39d14c4f3da017aab78e51806c9
Anshuman SINGH <118632749+ThakurAnshu23@users.noreply.github.com>
2023-09-14 16:57:10 +0530

    ESP32 led blink code

---
+++ led_blink.py
@@ -0,0 +1,13 @@
+from machine import Pin
+from time import sleep
+
+led = Pin(2,Pin.OUT)
+
+led_state = True
+
+while True:
+    led_state = not led_state
+    led.value(led_state)
+    print("LED turned on" if led_state else "LED turned off")
+    
+    sleep(0.1)
